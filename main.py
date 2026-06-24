d9e1f6308dfef054e7178f43cfa5a80f4d89b5b3
macarmo25ig-prog <macarmo25ig@epms.pt>
2026-06-24 10:17:40 +0000

    update

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import json
-import random 
-
+import random
 
 def mostrar_menu():
     while True:
-        print("----------* Quizz mais lindo! *----------")
+        print("----------* Pior quiz já feito *----------")
         print("1 - Jogar")
         print("2 - Regras")
         print("3 - Classificações")
         print("0 - Sair")
-        op = float(input("Introduz a tua opção: "))
+        op = int(input("Introduz a tua opção: "))
 
         if op == 0:
-            break
-        if op >= 1 and op <= 3:
+            break  
+        elif op == 2:
+            regras()
+        elif op == 3:
+            print("Aqui vai ficar a classificação")
+        else:
             return op
         
 def carregar_perguntas():
@@ -41,27 +44,30 @@ def verifica_resposta(pergunta, resposta):
         return True
     else:
         return False
+    
+def regras():
+    print("------------------------------------*   Regras   *-----------------------------------------")
+    print("\n1. Não pode selecionar algo fora das opções que tem de respota na hora que estiver jogando\n"   )
+    print("2. Você ganha mais pontos depedendo da dificuldade da pergunta que acertar, sendo elas:")
+    print("-- Facil +1 -- Medio +2 -- Dificil +3 --")
+    print("\n3. toda vez que iniciar sua pontuação vai ser salva para vc poder ver dps no menu de classificação\n")
+    input("Para sair aperte ENTER...")
 
 def main():
     op = mostrar_menu()
     if op == 1:
-        print("Aqui vai ficar a função de jogar")
-    elif op == 2:
-        print("Aqui vão ficar as regras")
-    elif op == 3:
-        print("Aqui vai ficar a classificação")
-    elif op == 1.5:
-        print(1)
+        p = carregar_perguntas()
+
+        perguntas_selecionadas = random.sample(p, 15)
+
+        for pergunta in perguntas_selecionadas:
+            op = mostrar_pergunta(pergunta)
+            validacao = verifica_resposta(pergunta, op)
+            if validacao:
+                print(":O VOCÊ É UMA MAQUINAA!")
+            else:
+                print(":( Errouuuu")
 
 main()
-p = carregar_perguntas()
 
-perguntas_selecionadas = random.sample(p, 15)
 
-for pergunta in perguntas_selecionadas:
-    op = mostrar_pergunta(pergunta)
-    validacao = verifica_resposta(pergunta, op)
-    if validacao:
-        print(":O VOCÊ É UMA MAQUINAA!")
-    else:
-        print(":( Errouuuu")
